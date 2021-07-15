95411b12030a43e95a1cabc1f4f44b73303c1cbe
safiay <safiayusuff@gmail.com>
2021-07-15 10:23:44 +0100

    all of interface and business logic completed

--- interface.js
+++ interface.js
@@ -6,21 +6,30 @@ window.addEventListener('load', function () {
     const powerSavingModeDisplay = document.getElementById('PowerSavingModeDisplay')
     const reset = document.getElementById('reset')
     const powerSavingMode = document.getElementById('PowerSavingMode')
+    const usage = document.getElementById('usage')
 
     up.addEventListener('click', function () {
         temperature.innerText = thermostat.up()
+        usage.innerText = thermostat.usage()
     })
 
     down.addEventListener('click', function () {
         temperature.innerText = thermostat.down()
-    })
-    
-    reset.addEventListener('click', function () {
-        temperature.innerText = thermostat.reset()
+        usage.innerText = thermostat.usage()
     })
 
-    powerSavingMode.addEventListener('click', function() {
+    reset.addEventListener('click', function () {
+        temperature.innerText = thermostat.reset()
+        usage.innerText = thermostat.usage()
+    })
+
+    powerSavingMode.addEventListener('click', function () {
         thermostat.powerSavingMode()
         powerSavingModeDisplay.innerText = thermostat.getPowerSavingMode()
+
     })
+
+
+    // find the element we're using (get element with id) set the = to a variable
+    // we need to edit our up and down functions so that they run our usage function from thermostat class after temperature changed 
 })
