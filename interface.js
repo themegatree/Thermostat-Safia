8b3367a6d4877a90df0d7daf914cc189de2cf87b
Shivam Sheth <shivam@shethmail.co.uk>
2021-07-15 12:24:32 +0100

    All in jQuery

--- interface.js
+++ interface.js
@@ -1,42 +1,63 @@
 window.addEventListener('load', function () {
-    const thermostat = new Thermostat
-    const up = document.getElementById('up')
-    const down = document.getElementById('down')
-    const temperature = document.getElementById('temperature')
-    const powerSavingModeDisplay = document.getElementById('PowerSavingModeDisplay')
-    const reset = document.getElementById('reset')
-    const powerSavingMode = document.getElementById('PowerSavingMode')
-    const usage = document.getElementById('usage')
+    const thermostat = new Thermostat;
+    // const up = document.getElementById('up')
+    // const down = document.getElementById('down')
+    // const temperature = document.getElementById('temperature')
+    // const powerSavingModeDisplay = document.getElementById('PowerSavingModeDisplay')
+    // const reset = document.getElementById('reset')
+    // const powerSavingMode = document.getElementById('PowerSavingMode')
+    // const usage = document.getElementById('usage')
 
-    up.addEventListener('click', function () {
-        temperature.innerText = thermostat.up()
-        usage.innerText = thermostat.usage()
-    })
-
-    down.addEventListener('click', function () {
-        temperature.innerText = thermostat.down()
-        usage.innerText = thermostat.usage()
-    })
-
-    reset.addEventListener('click', function () {
-        temperature.innerText = thermostat.reset()
-        usage.innerText = thermostat.usage()
-    })
-
-    powerSavingMode.addEventListener('click', function () {
-        thermostat.powerSavingMode()
-        powerSavingModeDisplay.innerText = thermostat.getPowerSavingMode()
-
-    })
 
     $(document).ready(function () {
+
+        // up.addEventListener('click', function () {
+        //     temperature.innerText = thermostat.up()
+        //     usage.innerText = thermostat.usage()
+        // })
+
         $("#up").click(function () {
-            // click on this up button and reset will go missing. 
-            $("#reset").hide();
+            $("#temperature").text(thermostat.up());
+            $("#usage").text(thermostat.usage());
         });
+
+        // down.addEventListener('click', function () {
+        //     temperature.innerText = thermostat.down()
+        //     usage.innerText = thermostat.usage()
+        // })
+
+        $("#down").click(function(){
+            $("#temperature").text(thermostat.down());
+            $("#usage").text(thermostat.usage());
+          });
+
+        // reset.addEventListener('click', function () {
+        //     temperature.innerText = thermostat.reset()
+        //     usage.innerText = thermostat.usage()
+        // })
+
+        $("#reset").click(function() {
+            $("#temperature").text(thermostat.reset())
+            $("#usage").text(thermostat.usage());
+        });
+
+        // powerSavingMode.addEventListener('click', function () {
+        //     thermostat.powerSavingMode()
+        //     powerSavingModeDisplay.innerText = thermostat.getPowerSavingMode()
+        // })
+
+
+        $("#PowerSavingMode").click(function() {
+            thermostat.powerSavingMode()
+            $("#PowerSavingModeDisplay").text(thermostat.getPowerSavingMode())
+        });
+
     });
 
-    // find the element we're using (get element with id) set the = to a variable
-    // we need to edit our up and down functions so that they run our usage function from thermostat class after temperature changed 
-})
+    $(document).ready(function () {
 
+    });
+});
+
+// find the element we're using (get element with id) set the = to a variable
+    // we need to edit our up and down functions so that they run our usage function from thermostat class after temperature changed 
